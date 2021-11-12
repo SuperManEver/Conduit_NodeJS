6265d17b13d04b64d1ef3e21b25a88206cfbcb25
Nik Luparev <nikita.luparev@gmail.com>
2021-11-12 05:35:14 +0500

    refactor

--- models/index.js
+++ models/index.js
@@ -1,5 +1,11 @@
-const Article = require('./Article');
+const User = require('./models/User');
+const Article = require('./models/Article');
+const Tag = require('./models/Tag');
+const Comment = require('./models/Comments');
 
 module.exports = {
   Article,
+  User,
+  Tag,
+  Comment,
 };

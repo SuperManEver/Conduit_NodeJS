1a342eca18828a7d91d85c327a404e97eae41e54
Nik Luparev <nikita.luparev@gmail.com>
2021-11-12 17:05:53 +0500

    move models relations

--- models/index.js
+++ models/index.js
@@ -3,6 +3,44 @@ const Article = require('./models/Article');
 const Tag = require('./models/Tag');
 const Comment = require('./models/Comments');
 
+//RELATIONS:
+//1 to many relation between user and article
+User.hasMany(Article, {
+  onDelete: 'CASCADE',
+});
+Article.belongsTo(User);
+
+//many to many relation between article and taglist
+Article.belongsToMany(Tag, {
+  through: 'TagList',
+  uniqueKey: false,
+  timestamps: false,
+});
+Tag.belongsToMany(Article, {
+  through: 'TagList',
+  uniqueKey: false,
+  timestamps: false,
+});
+
+//One to many relation between Article and Comments
+Article.hasMany(Comment, { onDelete: 'CASCADE' });
+Comment.belongsTo(Article);
+
+//One to many relation between User and Comments
+User.hasMany(Comment, { onDelete: 'CASCADE' });
+Comment.belongsTo(User);
+
+//Many to many relation between User and User
+User.belongsToMany(User, {
+  through: 'Followers',
+  as: 'followers',
+  timestamps: false,
+});
+
+//favourite Many to many relation between User and article
+User.belongsToMany(Article, { through: 'Favourites', timestamps: false });
+Article.belongsToMany(User, { through: 'Favourites', timestamps: false });
+
 module.exports = {
   Article,
   User,

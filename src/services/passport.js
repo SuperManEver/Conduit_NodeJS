04dccfabb18429bf226518573f42e193a012bc46
Nikita Luparev <nikita.luparev@gmail.com>
2021-12-10 21:36:39 +0500

    remove old code

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,5 +1,4 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const passportJWT = require('passport-jwt');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
@@ -11,40 +10,6 @@ const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
 // @todo: remove 'local' strategy for passport.js
-
-// passport.use(
-//   new LocalStrategy(
-//     // {
-//     //   usernameField: 'email',
-//     // },
-//     async (email, password, done) => {
-//       try {
-//         console.log('LocalStrategy: ', email, password);
-
-//         const user = await User.findOne({
-//           where: {
-//             email,
-//           },
-//           raw: true,
-//         });
-
-//         User.comparePassword(password, user.password, (err, matched) => {
-//           if (err) {
-//             throw err;
-//           }
-
-//           if (matched) {
-//             done(null, user);
-//           } else {
-//             done(null, false, { message: 'Invalid username / password' });
-//           }
-//         });
-//       } catch (err) {
-//         done(null, false, { message: 'Invalid username / password' });
-//       }
-//     },
-//   ),
-// );
 
 passport.use(
   new JWTStrategy(
